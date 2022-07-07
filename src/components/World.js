0cd874cc6d5a178051fd2e8035648f9660f99dad
GucciHepcat52 <dallinbreen624@gmail.com>
2022-07-07 13:26:31 -0600

    updated world render

--- src/components/World.js
+++ src/components/World.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../App.css";
 
 export default function World() {
-  return <div className="game-container"></div>;
+  useEffect(() => {
+    let canvas = document.getElementsByClassName("game-canvas");
+    let ctx = canvas.getContext("2d");
+    let image = new Image();
+    image.onload = () => {
+      ctx.drawImage(image, 0, 0);
+    };
+    image.src = "/";
+  }, []);
+
+  return (
+    <div className="game-container">
+      <div className="game-canvas"></div>
+    </div>
+  );
 }
